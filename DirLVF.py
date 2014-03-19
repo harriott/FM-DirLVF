34b8c52148a7b94f113a83f0ac1cb5cdc5a6f16d
harriott <trohib@gmail.com>
2014-03-19 10:21:08 +0100

    first commit
    all working well

---
+++ DirLVF.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+# vim: set tw=79:
+
+"""
+Print to file an indented directory structure listing, ready for vim folding.
+
+Looking recursively in the current directory,
+print to file a vim modeline, and then entire directory contents,
+with filepaths visually compacted, and files listed all in one sentence.
+
+Requires these accompanying vimfiles:
+    ftplugin/dirlist.vim - for the vim folding
+    syntax/dirlist.vim
+        - for helpful syntax highlighting of the directory names
+
+Reason for this:
+    Handily folded, compact, easily searchable in vim,
+    archive list of a directory.
+"""
+import datetime
+import os
+import re
+import sys
+
+# Get the datetime:
+startd = datetime.datetime.now().isoformat(' ')
+
+# Get this script's directory path
+scriptdirpath = os.path.dirname(os.path.abspath(__file__))
+# and this script's directory name
+scriptdir = os.path.split(scriptdirpath)[1].replace(" ", "_")
+# and this script's filename without extension
+scriptfilename = (os.path.basename(os.path.splitext(sys.argv[0])[0]))
+# and prepare the output file from them:
+outfile = scriptdir + '_' + scriptfilename + '.txt'
+print('When this is done, you should open', outfile, '\n')
+
+
+def rec_ind_dirlist(dirTolist):
+    """ Prepare a recursive directory list,
+    with subnodes indented one space relative to parent nodes,
+    and no paths repeated
+    (ie each directory name appears just once, like a header).
+    Show a progress count while working."""
+    print('Looking at contents of', scriptdirpath, ':')
+    dlc = 0
+    # Initialise the list just with the base folder path, and date:
+    dirList = scriptdirpath+' - scanned '+startd
+    for root, folders, files in os.walk(dirTolist):
+        if root == '.':
+            dirList += '\n.\n'
+        else:
+#           dirList += '\n'+root
+            dirList += '\n'+re.sub(r'.*?\\', r'.\\', root)+'\n'
+        for file in files:
+            # Report a progress count:
+            dlc += 1
+            print('\r', dlc, end=' ')
+            dirList += ' ¦ '+file
+    return dirList
+
+# Create a file object for output:
+fo = open(outfile, 'w', encoding='utf-8')
+# Write vim modeline and the list to the file object (and close it):
+fo.write('vim: set ft=dirlist fdm=expr:\n\n'+rec_ind_dirlist('.'))
+fo.close()
---
+++ README.md
@@ -0,0 +1,16 @@
+DirLVF
+======
+
+A Python script that works recursively through the directory its in,
+listing all of the contents into a text file, ready for folded-up Vim viewing.
+
+requirements
+============
+
+To get the full benefit - folded and syntax highlighted in gVim -
+you will need in your installation of gVim:
+
+- ftplugin/dirlist.vim
+- syntax/dirlist.vim
+
+You can get them out of https://github.com/harriott/vimfiles 
